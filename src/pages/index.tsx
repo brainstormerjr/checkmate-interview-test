d5ab6905fc73244e29e4cf2c06216cc10d49797d
brainstormerjr <60263058+brainstormerjr@users.noreply.github.com>
2023-02-10 21:17:01 +0800

    Completed Task 1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import GoogleButton from 'react-google-button'
-import {getAuth, GoogleAuthProvider} from "firebase/auth";
+import {getAuth, GoogleAuthProvider, signInWithRedirect, getRedirectResult} from "firebase/auth";
 import {useRouter} from "next/router";
 import { initializeApp } from 'firebase/app';
 
@@ -8,19 +8,50 @@ import { initializeApp } from 'firebase/app';
 // Replace the following with your app's Firebase project configuration
 // https://firebase.google.com/docs/web/setup
 const firebaseConfig = {
-  // Enter your own firebase config here
+  apiKey: "AIzaSyBPgUclU83FPeIGdRqr9UTw_yhhAhlCQBc",
+  authDomain: "checkmate-interview-test.firebaseapp.com",
+  projectId: "checkmate-interview-test",
+  storageBucket: "checkmate-interview-test.appspot.com",
+  messagingSenderId: "585295346001",
+  appId: "1:585295346001:web:f516df441fe0c980cf3b70"
 };
 
-// const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
 // GoogleAuthProvider instance
-// const provider = new GoogleAuthProvider();
+const provider = new GoogleAuthProvider();
 // Firebase Auth instance
-// const auth = getAuth(app);
+const auth = getAuth(app);
 
 export default function Home() {
   //Next.js router
   const router = useRouter();
+
+  // Get the redirect result to see if the user is signed-in
+  getRedirectResult(auth).then(result => {
+
+    console.log(result);
+
+    if (!result) return; // User is NOT signed in, there is no result. Do not redirect
+
+    // Get Google Access Token
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+
+    if (!credential) return;
+
+    const token = credential.accessToken;
+
+    // Get user info
+    const user = result.user;
+
+    // Since user is signed in, redirect to signed-in page
+    router.push('signed-in');
+
+  }).catch(err => {
+
+    console.error(err);
+
+  });
 
   // Task 1: Implement Google Sign in with Firebase
   // https://firebase.google.com/docs/auth/web/google-signin
@@ -32,6 +63,9 @@ export default function Home() {
       4. Redirect the user to the signed-in page using Next.js router
      */
 
+    // Sign in with redirect
+    signInWithRedirect(auth, provider);
+    // After signing in, getRedirectResult() will yield a result and we can redirect to signed-in page
   }
 
   return (
