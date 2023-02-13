9b1da6c5e39592b2f76dc8df3208e3f170d7b634
brainstormerjr <60263058+brainstormerjr@users.noreply.github.com>
2023-02-14 00:26:07 +0800

    Draft Task 3

--- src/pages/signed-in.tsx
+++ src/pages/signed-in.tsx
@@ -10,14 +10,51 @@ interface Joke {
 export default function SignedIn({ joke }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   console.log(joke);
   return (
-    <div>
-      <h1>Signed In</h1>
-      <h2>Wanna hear a programming joke?</h2>
-      <h2>{ joke.setup }</h2>
-      <h2>{ joke.punchline }</h2>
-      {/* Task 3: Your own presentation of the joke here (Free Style 😉 )*/}
+    <div className="signed-in">
+      <h1>CHECKMATE</h1>
+      <h2>work hard + play hard</h2>
+      <div className="window-title-bar">
+        <div className="window-buttons">
+          <span className="red">⬤</span> &nbsp;
+          <span className="yellow">⬤</span> &nbsp;
+          <span className="green">⬤</span>
+          </div>
+        CheckMate Joke Terminal
+      </div>
+      <div className="joke-display">
+        <h1>Last login: {
+          Date().split(" ")[0] + " " +        // Day of the week
+          Date().split(" ")[1] + " " +        // Month
+          Date().split(" ")[2] + " " +        // Day
+          Date().split(" ")[3] + " " +        // Year
+          Date().split(" ")[4].substring(0,5) // hh:mm(:ss)
+          // No using seconds, or a react hydration error may occur
+          // https://nextjs.org/docs/messages/react-hydration-error
+        }</h1>
+        <h1>
+          signed-in@
+          <a href="https://checkmatehk.io" className="blue">CheckMate </a>
+          ~ %
+          <span> JOKE ./programming/random/</span>
+        </h1>
+        {/* &nbsp; is a space */}
+        <h1>
+          <span className="bg-red">&nbsp;SETUP </span>
+          &nbsp;{joke.setup}
+        </h1>
+        <h1>
+          <span className="bg-green">&nbsp;PUNCHLINE </span>
+          &nbsp;{joke.punchline}
+        </h1>
+        <h1>
+          signed-in@
+          <a href="https://checkmatehk.io" className="blue">CheckMate </a>
+          ~ % ▌
+        </h1>
+        {/* Task 3: Your own presentation of the joke here (Free Style 😉 )*/}
 
-      {/* End of Task 3 */}
+        {/* End of Task 3 */}
+      </div>
     </div>
   )
 
