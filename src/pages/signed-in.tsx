f76904654727716177fc282182ee22b41afd4a10
brainstormerjr <60263058+brainstormerjr@users.noreply.github.com>
2023-02-13 18:06:10 +0800

    Completed Task 2

--- src/pages/signed-in.tsx
+++ src/pages/signed-in.tsx
@@ -1,9 +1,20 @@
 import {GetServerSideProps, InferGetServerSidePropsType} from "next";
 
+interface Joke {
+  type:string;
+  setup:string;
+  punchline:string;
+  id:number;
+}
+
 export default function SignedIn({ joke }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  console.log(joke);
   return (
     <div>
       <h1>Signed In</h1>
+      <h2>Wanna hear a programming joke?</h2>
+      <h2>{ joke.setup }</h2>
+      <h2>{ joke.punchline }</h2>
       {/* Task 3: Your own presentation of the joke here (Free Style 😉 )*/}
 
       {/* End of Task 3 */}
@@ -17,11 +28,23 @@ export default function SignedIn({ joke }: InferGetServerSidePropsType<typeof ge
 export const getServerSideProps: GetServerSideProps = async (context) => {
   // Fetch data from external API and pass it to the page via props.joke
 
+  const url = "https://official-joke-api.appspot.com/jokes/programming/random";
+  const response = await fetch(url);
+  const data = await response.json();
+  let apiJoke:Joke 
+  if (data) apiJoke = data[0];
+  else {
+    apiJoke = {
+      type: "programming",
+      setup: "legen- wait for it...",
+      punchline: "dary!",
+      id: -1,
+    }
+  }
+
   return {
     props: {
-      joke: {
-
-      },
+      joke: apiJoke,
     }, // will be passed to the page component as props
   }
 }
